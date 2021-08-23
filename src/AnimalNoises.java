27e0fde4fabae4038c6066fce49e360dcf48d8bf
Kelli K <kjkleindorfer@dmacc.edu>
2021-08-23 14:54:22 -0500

    Initial Upload
    Add Cat and initial AnimalNoises files

---
+++ src/AnimalNoises.java
@@ -0,0 +1,14 @@
+import model.Cat;
+
+public class AnimalNoises {
+
+	// add your animal class to the model package
+	// only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Cat fluffy = new Cat();
+		System.out.println(fluffy.speak());
+
+	}
+
+}
