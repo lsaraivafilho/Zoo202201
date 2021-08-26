3c60447f94a33a2ac8d5a1ce497cd39dd6f7ca7a
church519 <church519@gmail.com>
2021-08-26 18:36:04 -0500

    Added Giraffe class, added Giraffe instance

--- src/AnimalNoises.java
+++ src/AnimalNoises.java
@@ -1,5 +1,6 @@
 import model.Cat;
 import model.Dog;
+import model.Giraffe;
 import model.Snake;
 public class AnimalNoises {
 
@@ -15,6 +16,9 @@ public class AnimalNoises {
 		
 		Dog spot = new Dog();
 		System.out.println(spot.speak());
+		
+		Giraffe craig = new Giraffe();
+		System.out.println(craig.speak());
 	}
 
 }
